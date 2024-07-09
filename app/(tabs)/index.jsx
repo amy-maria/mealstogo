660054acb4d528a048a080f4fb8ab8c27dbbd332
amy-maria <122695916+amy-maria@users.noreply.github.com>
2024-07-09 18:53:18 -0400

    added Card and styled components

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { StatusBar as ExposStatusBar } from 'react-native-screens';
+import { RestaurantsScreen } from '@features/restaurants/screens/restaurantscreens.jsx';
+
+export default function App() {
+  return (
+    <>
+      <RestaurantsScreen />
+    </>
+  );
+}
